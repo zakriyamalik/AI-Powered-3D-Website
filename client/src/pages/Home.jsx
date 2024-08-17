5a8c0b0145736388a13d5653ab72267d8f15fdd8
Muhammad Zakriya <zakriyamalik852@gmail.com>
2024-08-17 17:37:57 +0500

    Setting up Base of Project and Logo

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {motion,AnimatePresence} from 'framer-motion'
+import { useSnapshot } from 'valtio' 
+import  state  from '../store';
+import{
+    headContainerAnimation,
+    headContentAnimation,
+    headTextAnimation,
+    slideAnimation
+} from '../config/motion'
+
+const Home = () => {
+    const snap= useSnapshot(state);
+  return (
+    <AnimatePresence>
+        {
+            snap.intro &&(
+                <motion.section className="home" {...slideAnimation('left')}>
+                    <motion.header {...slideAnimation('down')}>
+                        <img
+                        src='./threejs.png'
+                        alt="logo"
+                        className='w-8 h-8 object-contain'/>
+                    </motion.header>
+                </motion.section>
+            )
+        }
+    </AnimatePresence>
+  )
+}
+
+export default Home
