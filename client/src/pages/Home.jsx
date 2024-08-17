af2743b9abbd0503da6438191302821700f50cb0
Muhammad Zakriya <zakriyamalik852@gmail.com>
2024-08-17 18:27:24 +0500

    Home Page Basic

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {motion,AnimatePresence} from 'framer-motion'
 import { useSnapshot } from 'valtio' 
 import  state  from '../store';
+import { CustomButton } from '../components';
 import{
     headContainerAnimation,
     headContentAnimation,
@@ -22,6 +23,27 @@ const Home = () => {
                         alt="logo"
                         className='w-8 h-8 object-contain'/>
                     </motion.header>
+                    <motion.div {...headTextAnimation}>
+                        <h1 className="head-text">
+                            LET'S <br className='xl:block hidden' />DO IT. 
+                        </h1>
+                    </motion.div>
+                    <motion.div {...headTextAnimation}
+                    className='flex flex-col gap-5'
+                    >
+                       <p className='max-w-md font-normal text-grey-600 text-base'>
+                        Create your unique and exclusive shirt with our brand-new 3D customiztion tool. <strong>
+                            Unleash your imagination 
+                        </strong> {" "}
+                        and define your own style.
+                       </p>
+                        <CustomButton type="filled"
+                        title="Customize It"
+                        handleClick={()=> state.intro=false}
+                        customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                        />
+                    </motion.div>
+
                 </motion.section>
             )
         }
