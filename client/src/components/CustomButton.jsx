971ce3753954ed998e26172fd02e0513f7b0092a
Muhammad Zakriya <zakriyamalik852@gmail.com>
2024-08-18 16:55:37 +0500

    button

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -15,7 +15,7 @@ const CustomButton=({type,title,customStyles,handleClick})=> {
     }
   return (
     <button className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`} style={generateStyle(type)}
-    onClick={handleClick}>
+    onClick={handleClick}> 
         {title}
 
     </button>
