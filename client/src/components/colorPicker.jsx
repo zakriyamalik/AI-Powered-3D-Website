c872499794cebdb3b1f7891de800c79e0d0a11a4
Muhammad Zakriya <zakriyamalik852@gmail.com>
2024-08-20 23:02:28 +0500

    Color Picker Tab Completed!!!

--- client/src/components/colorPicker.jsx
+++ client/src/components/colorPicker.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import {SketchPicker} from 'react-color'
+import { useSnapshot } from 'valtio'
+import state from '../store'
 const ColorPicker = () => {
+  const snap = useSnapshot(state);
+  
   return (
-    <div>
-      cooler
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+      color={snap.color}
+      disableAlpha
+      const presetColors = {[
+        '#FF7F50', // Coral
+        '#40E0D0', // Turquoise
+        '#FA8072', // Salmon
+        '#F0E68C', // Khaki
+        '#DDA0DD', // Plum
+        '#ADD8E6'  // Light Blue
+      ]}
+      
+      onChange={(color)=> state.color=color.hex}
+      />
     </div>
   )
 }
